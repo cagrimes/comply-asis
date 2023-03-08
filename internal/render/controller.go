0792410f697beae31f55c21429f674cb20047898
Charles Grimes <retail@charlesgrimes.net>
2023-03-08 12:26:04 -0500

    Update controller.go
    Update to cagrimes/comply-asis

--- internal/render/controller.go
+++ internal/render/controller.go
@@ -6,8 +6,8 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
-	"github.com/strongdm/comply/internal/config"
-	"github.com/strongdm/comply/internal/model"
+	"github.com/scagrimes/comply-asis/internal/config"
+	"github.com/cagrimes/comply-asis/internal/model"
 )
 
 type project struct {
