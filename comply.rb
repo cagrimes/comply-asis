d708d49d7e6793f9dbce5c1707750718cd1f57aa
Charles Grimes <retail@charlesgrimes.net>
2023-03-08 11:40:29 -0500

    Update comply.rb
    Updated for cagrimes/compy-asis 
    
    url "file/Users/iassco/Downloads/comply-asis-Homebrew"

--- comply.rb
+++ comply.rb
@@ -1,22 +1,37 @@
-class Comply < Formula
-    desc ""
-    homepage ""
-    url "file:///Users/jmccarthy/Downloads/comply-1.1.3.tgz"
-    sha256 "01f9920e5e9fbd29386e4a4131fac78c002730e49c3f870a0ee2b958c3ce75f6"
+class ComplyAsis < Formula
+  desc "SOC policy builder"
+  homepage "http://iassecurity.net"
+  url "file://Users/iassco/Downloads/comply-asis-Homebrew.tar"
+  version "1.0"
+  sha256 "07d106a5f2040200cecce419f77426b9e667c822a5266997955789ca0b2915ba"
+  license "Apache-2.0"
 
-    depends_on "go" => :build
+  depends_on "cmake" => :build
+  depends_on "go" => :build
 
-    def install
-        ENV["GOPATH"] = buildpath
-        ENV.prepend_create_path "PATH", buildpath/"bin"
-        (buildpath/"src/github.com/strongdm/comply").install buildpath.children
-        cd "src/github.com/strongdm/comply" do
-            system "make", "brew"
-            bin.install "bin/comply"
-        end
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV.prepend_create_path "PATH", buildpath/"bin"
+    (buildpath/"src/github.com/cagrimes/comply-asis").install buildpath.children
+    cd "src/github.com/cagrimes/comply-asis" do
+      system "make", "brew"
+      bin.install "bin/comply-asis"
+
+      # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+      # system "./configure", *std_configure_args, "--disable-silent-rules"
     end
+  end
 
-    test do
-        system "#{bin}/sdm", "logout"
-    end
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test comply-asis`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
 end
